eff0ea7e9f1ae4e68b2d4b519e653579f1a031d9
v0id-X <rachitroykr@gmail.com>
2025-10-16 23:10:21 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:process.env.VITE_BASE_PATH  || '/the-film-list',
   server:{
     proxy:{
       '/api':{
